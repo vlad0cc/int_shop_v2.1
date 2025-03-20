5e2ef6439684e43e02d811aec2cb11fdd9fb067c
Vladislav <97551004+vlad0cc@users.noreply.github.com>
2025-03-20 14:59:59 +0300

    Update request.go

--- go-pet-project-master 2/payment/APIrequest/request.go
+++ go-pet-project-master 2/payment/APIrequest/request.go
@@ -23,6 +23,7 @@ type {
 	func (a SortBy) Len() int           { return len(a) }
 	func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 	func (a SortBy) Less(i, j int) bool { return a[i] < a[j] }
+	889879978
 }
 
 type CheckoutRequest struct {
